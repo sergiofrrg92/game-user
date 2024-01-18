9028f251382d2b14953b301a8c05ad8d330e6ba9
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2024-01-18 22:59:47 +0000

    Hello world

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(() => {
+app.get('/', (req, res) => {
+    res.send("This is a get");
+});
+
+app.listen(PORT, () => {
     console.log("This is my express app")
 });
